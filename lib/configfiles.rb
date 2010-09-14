2a7c3437eb7581b90382dc6ec685c5c937712846
Guido De Rosa <job@guidoderosa.net>
2010-09-14 16:49:30 +0200

    cleanup

--- lib/configfiles.rb
+++ lib/configfiles.rb
@@ -23,7 +23,7 @@ module ConfigFiles
     class ValidationFailed        < ::RuntimeError;   end
     class AlreadyDefinedParameter < ::Exception;      end
     class DefaultAlreadySet       < ::Exception;      end
-
+    
     AlreadyDefinedDefault = DefaultAlreadySet
 
     @@parameters  ||= {}
@@ -36,111 +36,110 @@ module ConfigFiles
     }
     @@validate    ||= lambda {|data| true} 
 
-    # Examples: 
-    #   on :unknown_parameter, :fail # or :accept, or :ignore
-    #   on :unknown_parameter, {|str| str.to_i}
-    #
-    # There's also :unknown_value, to specify behavior when the
-    # converter is an Hash and the value found if not among the
-    # hash keys. Usage is similar.
-    #
-    def self.on(circumstance, action=nil, &block)
-      actions       = [:accept, :ignore, :fail] 
-      circumstances = [:unknown_parameter, :unknown_value]      
-      unless circumstances.include? circumstance
-        raise ArgumentError, "Invalid circumstance: #{circumstance.inspect}. Allowed values are #{circumstances.list_inspect}."
-      end
-      if block
-        @@behavior[circumstance] = block
-      elsif action == :accept
-        @@behavior[circumstance] = lambda {|x| x} 
-      elsif actions.include? action
-        @@behavior[circumstance] = action
-      else
-        raise ArgumentError, "Invalid action: #{action}. Allowed values are #{actions.list_inspect}."
-      end
-    end
+    class << self
 
-    def self.option(name)
-      @@behavior[name]
-    end
-
-    def self.parameter(name, converter=nil, &converter_block)
-      if @@parameters[name] and @@parameters[name][:converter]
-        raise AlreadyDefinedParameter, "Already defined parameter \"#{name}\""
-      end
-      if converter
-        if converter_block
-          raise ArgumentError, 'you must either specify a symbol or a block'
-        elsif converter.is_a? Hash
-
-          converter_block = lambda do |x| # x is a String from conf file 
-            if converter.keys.include? x
-              return converter[x] # returns from lambda, not from method 
-            elsif @@behavior[:unknown_value] == :fail
-              raise ArgumentError, "Invalid value \"#{x}\" for parameter \"#{name}\". Allowed values are #{converter.keys.list_inspect}."
-            elsif @@behavior[:unknown_value] == :accept
-              return x
-            end
-          end 
-
-        else #Symbol
-          converter_block = lambda {|x| x.method(converter).call}
-        end
-      else
-        converter_block ||= lambda {|x| x}  
-      end
-      @@parameters[name] ||= {} 
-      @@parameters[name][:converter] = converter_block
-    end
-
-    def self.default(name, value)
-      if @@parameters[name] and @@parameters[name][:default]
-        raise DefaultAlreadySet, "Default for \"#{name}\" has been already set (to value: #{@@parameters[name][:default]})"
-      end
-      @@parameters[name] ||= {}
-      @@parameters[name][:default] = value
-    end
-
-    # A special kind of parameter, with a special kind of converter, 
-    # which in turn
-    # converts an Enumerable yielding Strings into an Enumerator of 
-    # custom objects. You may work with Enumerators instead of
-    # Arrays , which is the right thing to do when you deal with very 
-    # long list of names, IP adresses, URIs etc (lazy evaluation) .
-    def self.enumerator(name, converter=nil, &converter_block)
-      if block_given?
-        raise ArgumentError, 'you must either specify a symbol or a block' if
-            converter
-      else
-        if converter # converter may be :to_i etc.
-          converter_block = lambda {|x| x.method(converter).call} 
-        else
-          converter_block = lambda {|x| x}
-        end
-      end
-      #parameter name do |enumerable|
-      #  Enumerator.new do |yielder|
-      #    enumerable.each do |string|
-      #      yielder << converter_block.call(string) 
-      #    end
-      #  end
-      #end
+      # Examples: 
+      #   on :unknown_parameter, :fail # or :accept, or :ignore
+      #   on :unknown_parameter, {|str| str.to_i}
       #
-      # Use facets instead
-      parameter name do |enumerable|
-        enumerable.defer.map{|element| converter_block.call(element)} 
+      # There's also :unknown_value, to specify behavior when the
+      # converter is an Hash and the value found if not among the
+      # hash keys. Usage is similar.
+      #
+      def on(circumstance, action=nil, &block)
+        actions       = [:accept, :ignore, :fail] 
+        circumstances = [:unknown_parameter, :unknown_value]      
+        unless circumstances.include? circumstance
+          raise ArgumentError, "Invalid circumstance: #{circumstance.inspect}. Allowed values are #{circumstances.list_inspect}."
+        end
+        if block
+          @@behavior[circumstance] = block
+        elsif actions.include? action
+          @@behavior[circumstance] = action
+        elsif action
+          raise ArgumentError, "Invalid action: #{action}. Allowed values are #{actions.list_inspect}."
+        else
+          return @@behavior[circumstance] 
+        end
       end
-    end
 
-    def self.validate(&block)
-      @@validate = block
-    end
+      def behavior_on(circumstance); on(circumstance); end
 
-    attr_accessor :options #, :data
+      def parameter(name, converter=nil, &converter_block)
+        if @@parameters[name] and @@parameters[name][:converter]
+          raise AlreadyDefinedParameter, "Already defined parameter \"#{name}\""
+        end
+        if converter
+          if converter_block
+            raise ArgumentError, 'you must either specify a symbol or a block'
+          elsif converter.is_a? Hash
+
+            converter_block = lambda do |x| # x is a String from conf file 
+              if converter.keys.include? x
+                return converter[x] # returns from lambda, not from method 
+              elsif @@behavior[:unknown_value] == :fail
+                raise ArgumentError, "Invalid value \"#{x}\" for parameter \"#{name}\". Allowed values are #{converter.keys.list_inspect}."
+              elsif @@behavior[:unknown_value] == :accept
+                return x
+              end
+            end 
+
+          else #Symbol
+            converter_block = lambda {|x| x.method(converter).call}
+          end
+        else
+          converter_block ||= lambda {|x| x}  
+        end
+        @@parameters[name] ||= {} 
+        @@parameters[name][:converter] = converter_block
+      end
+
+      def default(name, value)
+        if @@parameters[name] and @@parameters[name][:default]
+          raise DefaultAlreadySet, "Default for \"#{name}\" has been already set (to value: #{@@parameters[name][:default]})"
+        end
+        @@parameters[name] ||= {}
+        @@parameters[name][:default] = value
+      end
+
+      # A special kind of parameter, with a special kind of converter, 
+      # which in turn
+      # converts an Enumerable yielding Strings into an Enumerator of 
+      # custom objects. You may work with Enumerators instead of
+      # Arrays , which is the right thing to do when you deal with very 
+      # long list of names, IP adresses, URIs etc (lazy evaluation) .
+      def enumerator(name, converter=nil, &converter_block)
+        if block_given?
+          raise ArgumentError, 'you must either specify a symbol or a block' if
+              converter
+        else
+          if converter # converter may be :to_i etc.
+            converter_block = lambda {|x| x.method(converter).call} 
+          else
+            converter_block = lambda {|x| x}
+          end
+        end
+        #parameter name do |enumerable|
+        #  Enumerator.new do |yielder|
+        #    enumerable.each do |string|
+        #      yielder << converter_block.call(string) 
+        #    end
+        #  end
+        #end
+        #
+        # Use facets instead
+        parameter name do |enumerable|
+          enumerable.defer.map{|element| converter_block.call(element)} 
+        end
+      end
+
+      def validate(&block)
+        @@validate = block
+      end
+
+    end # class << self
 
     def initialize
-      @behavior = @@behavior.dup
       @data = {}
     end
 
@@ -153,10 +152,12 @@ module ConfigFiles
       h.each_pair do |id, value|
         if @@parameters[id] and @@parameters[id][:converter]
           @data[id] = @@parameters[id][:converter].call(value)
-        elsif @behavior[:unknown_parameter] == :fail
+        elsif @@behavior[:unknown_parameter] == :fail
           raise RuntimeError, "unknown parameter #{key}" # otherwise ignore
-        elsif @behavior[:unknown_parameter].respond_to? :call
-          block = @behavior[:unknown_parameter]
+        elsif @@behavior[:unknown_parameter] == :accept
+          @data[id] = value
+        elsif @@behavior[:unknown_parameter].respond_to? :call
+          block = @@behavior[:unknown_parameter]
           @data[id] = block.call value
         end
       end
@@ -168,32 +169,29 @@ module ConfigFiles
         end
       end
 
-      @data.merge! deferred_data_h
+      @data.merge! deferred_data
 
       validate  
     end
 
-    # behave like a Hash, but more rigidly! you cannot automagically
-    # add keys without calling Configfiles::Base.parameter
-    
-    def data(key)
+    # Like a Hash#[], but more rigidly! you cannot automagically
+    # add keys without calling Configfiles::Base::parameter
+
+    def [](key)
       if @data.keys.include? key
         @data[key]
       else
-        raise NoKeyError, "uknown key '#{key}' for #{self.class}"
+        raise NoKeyError, "unknown key '#{key}' for #{self.class}"
       end
     end
-    def get(key); data(key);  end
-    def [](key);  data(key);  end 
 
-    def set(key, val)
+    def []=(key, val)
       if @data.keys.include? key
         @data[key] = val
       else
         raise NoKeyError, "uknown key '#{key}' for #{self.class}"
       end
     end
-    def []=(key, val); set(key, val); end
 
     def each(&blk) 
       @data.each(&blk)
@@ -201,14 +199,14 @@ module ConfigFiles
 
     private
 
-    def deferred_data_h
-      results_h = {} 
+    def deferred_data
+      results = {} 
       @data.each do |k, v|
         if v.is_a? Proc
-          results_h[k] = v.call(@data)
+          results[k] = v.call(@data)
         end
       end
-      results_h
+      results
     end
 
   end
